792222a964a780e4e8a83319ac536e3190bc20ea
Juejuesun <48380934+Juejuesun@users.noreply.github.com>
2019-09-29 01:38:03 +0800

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,22 @@
+// function func(){
+//     var ns = new Date();
+//     document.getElementById("aa").innerHTML = ns;
+// }
+
+// function fun(v) {  
+//     var bb=$(v).value;  
+//     window.alert(v+ "=" +bb);  
+//     window.alert('end');    
+//     document.getElementById("ja").innerHTML = bb;  
+// }
+
+function bs(){
+    var ti = new Date ();
+    document.getElementById("dem").innerHTML = ti;
+}
+function a(){
+    var age;
+    age=document.getElementById("demo").value;
+    var d=new Date ();
+    document.getElementById("demo").innerHTML = d;
+}  
